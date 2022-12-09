0a44b83c2fd57d292a2d7b4e677bc0116b3e8156
HyeonGyu <yhu930421@naver.com>
2022-12-09 16:30:56 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React s
+          Learn React
         </a>
       </header>
     </div>
