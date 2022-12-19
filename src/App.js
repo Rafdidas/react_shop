c612ad4e1571863dc84b4a5d4b89980a36de754b
HyeonGyu <yhu930421@naver.com>
2022-12-19 16:20:25 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='#wrapper'>
+        <header id='header'>
+          <div className='gnb'>
+            <div className='wrap_inner_hd'>
+              <h1 className='title'><img src={process.env.PUBLIC_URL + './img/lesop_logo.png'}/></h1>
+              <ul className='hd_menu'>
+                <li><a href="#">모든 제품</a></li>
+                <li><a href="#">브랜드 소개</a></li>
+                <li><a href="#">프로모션</a></li>
+                <li><a href="#">커뮤니티</a></li>
+                <li><a href="#">고객센터</a></li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
